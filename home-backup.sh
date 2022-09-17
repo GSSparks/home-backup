335f62f233a6655013670a406e712229d715011f
Gary Sparks <112287133+GSSparks@users.noreply.github.com>
2022-09-17 08:55:08 -0400

    Update home-backup.sh

--- home-backup.sh
+++ home-backup.sh
@@ -4,25 +4,30 @@ TIME=`date +%b-%d-%y`
 USER=`whoami` # Using current user's Home directory
 LOGFILE=/home/$USER/bu_log_$TIME
 
-HONE_DIR='/home/$USER'
-HOME_STAT='/home/$USER/.home_stat'
+HOME_DIR='/home/$USER'
 HOME_BU_DIR=$1
+DIRECTORY=("Documents" "Pictures" "Videos")
 
-if [ -e $HOME_STAT ] # Is there a stat file for home?
-then
-	HOME_STAT=`cat $HOME_STAT`
-else
-	HOME_STAT='nothing'
-fi
+for i in $DIRECTORY; do
+	
+	DIR_STAT="/home/$USER/.$i_stat"
+	
+	if [ -e $DIR_STAT ] # Is there a stat file for the directory?
+	then
+		DIR_STAT=`cat $DIR_STAT`
+	else
+		DIR_STAT=''
+	fi
 
-NEW_HOME_STAT=`stat -t $HOME_DIR`
+	NEW_DIR_STAT=`stat -t $HOME_DIR/$i`
 
-if [ "$HOME_STAT" != "$NEW_HOME_STAT" ]
-then
-	echo 'Changes have been detected. Backing up Home!' >> $LOGFILE
-	HOME_BU_FILENAME=$USER_bu_$TIME.tar.gz
-	tar -cvpzf $HOME_BU_DIR/$HOME_BU_FILENAME $HOME_DIR 2>> $LOGFILE
-	echo $NEW_HOME_STAT > $HOME_STAT
-else
-	echo "No changes have been detected. Not Backing up $USER's Home directory." >> $LOGFILE
-fi
+	if [ "$DIR_STAT" != "$NEW_DIR_STAT" ]
+	then
+		echo "Changes have been detected. Backing up $i!" >> $LOGFILE
+		HOME_BU_FILENAME=$USER_$i_bu_$TIME.tar.gz
+		tar -cvpzf $HOME_BU_DIR/$HOME_BU_FILENAME $HOME_DIR 2>> $LOGFILE
+		echo $NEW_HOME_STAT > $HOME_STAT
+	else
+		echo "No changes have been detected. Not Backing up $i directory." >> $LOGFILE
+	fi
+done
